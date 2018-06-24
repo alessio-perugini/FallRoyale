8f8f0b9abdebd939c0ae6841a419b6fb18f1a1b2
Alessio <xxxredfuegolight@gmail.com>
2018-06-24 18:06:04 +0200

    init query ruota della fortuna

--- lista_query.sql
+++ lista_query.sql
@@ -254,3 +254,44 @@ ALTER TABLE utenti ADD COLUMN referral TEXT;
 -- modifica reedem con aggiunta di riscattare le skin
 ALTER TABLE codici ADD COLUMN skin_fk INT;
 ALTER TABLE codici ADD CONSTRAINT fk_codici_skin_fk FOREIGN KEY (skin_fk) REFERENCES items(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+
+-- dev 1.4.0
+CREATE TABLE ruote(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255),
+    descrizione TEXT,
+    data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE premi(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    valuta INT,
+    tipo_valuta INT,
+    id_item_fk INT,
+
+    CONSTRAINT fk_premi_id_item FOREIGN KEY (id_item_fk) REFERENCES items(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE spicchi_ruote(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_ruota_fk INT,
+    id_premio_fk INT,
+
+    CONSTRAINT fk_spicchi_ruota_fk FOREIGN KEY (id_ruota_fk) REFERENCES ruote(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_spicchi_id_premio_fk FOREIGN KEY (id_premio_fk) REFERENCES premi(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE log_ruota(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    data_spin TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_ruota_fk INT,
+    id_premio_vinto_fk INT,
+    id_utente_fk INT,
+
+    CONSTRAINT fk_log_ruota_id_ruota_fk FOREIGN KEY (id_ruota_fk) REFERENCES ruote(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_log_ruota_id_premio_vinto_fk FOREIGN KEY (id_premio_vinto_fk) REFERENCES premi(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_log_ruota_id_utente_fk FOREIGN KEY (id_utente_fk) REFERENCES utenti(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
